cc96773242cf9fb8e98f94b562a13f021c06a79a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-08-05 17:20:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ configure do
+    set :views, "app/views"
+    set :public_dir, "public"
+  end
+ 
  get '/' do
    erb :index 
  end 
